0883a9dcf3c8cc840af90cb103b0c965ccefe4d7
Brad <28307684+mad-briller@users.noreply.github.com>
2022-11-08 15:18:40 +0000

    add broken code.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Console\Events\CommandStarting;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->make(Dispatcher::class)
+                  ->listen(CommandStarting::class, function ($event) {
+                      dd(
+                          $event->input->getArgument('arg'), // 'test:command'
+                          $event->input->getArgument('arg1'), // value of `arg`
+                      );
+                  });
     }
 
     /**
